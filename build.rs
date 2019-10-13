3c2836dda30327b67f49085561ae4ce275bdf373
Jay Schmidek <jay@schmidek.ca>
2019-10-12 22:35:45 -0700

    Initial attempt at static build

--- build.rs
+++ build.rs
@@ -1,12 +1,63 @@
 // Knockway Inc. and its affiliates. All Rights Reserved
 
 extern crate bindgen;
+extern crate cc;
 
 use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    println!("cargo:rustc-link-lib=dylib=postal");
+    let sources = vec!(
+        "strndup.c",
+        "libpostal.c",
+        "expand.c",
+        "address_dictionary.c",
+        "transliterate.c",
+        "tokens.c",
+        "trie.c",
+        "trie_search.c",
+        "trie_utils.c",
+        "string_utils.c",
+        "file_utils.c",
+        "utf8proc/utf8proc.c",
+        "normalize.c",
+        "numex.c",
+        "features.c",
+        "unicode_scripts.c",
+        "address_parser.c",
+        "address_parser_io.c",
+        "averaged_perceptron.c",
+        "crf.c",
+        "crf_context.c",
+        "sparse_matrix.c",
+        "averaged_perceptron_tagger.c",
+        "graph.c",
+        "graph_builder.c",
+        "language_classifier.c",
+        "language_features.c",
+        "logistic_regression.c",
+        "logistic.c",
+        "minibatch.c",
+        "float_utils.c",
+        "ngrams.c",
+        "place.c",
+        "near_dupe.c",
+        "double_metaphone.c",
+        "geohash/geohash.c",
+        "dedupe.c",
+        "string_similarity.c",
+        "acronyms.c",
+        "soft_tfidf.c",
+        "jaccard.c",
+        "klib/drand48.c",
+        "scanner.c",
+    );
+    let mut build = cc::Build::new();
+    for source in sources {
+        build.file(format!("libpostal/src/{}", source));
+    }
+    build.include("libpostal");
+    build.compile("libpostal.a");
 
     let bindings = bindgen::Builder::default()
         .rustfmt_bindings(true)
